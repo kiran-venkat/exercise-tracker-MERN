35e60ed1796ccc0e75b7f1a976eab1abb6467c16
kiran <kirven05@gmail.com>
2022-09-19 15:42:55 +0530

    first commit

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercises extends Component {
+  render() {
+    return (
+      <div> 
+        <p>You are on edit exercises component!</p>
+      </div>
+    )
+  }
+}
