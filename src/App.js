5baca66622c2866351854972f7cc42fd0c8a2267
Kiran <54139318+kiran-venkat@users.noreply.github.com>
2022-09-19 22:52:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
+
+
 function App() {
   return (
     <Router>
